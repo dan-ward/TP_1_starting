5d12bb6d8e5252549146b4bf758613a0dd49dd82
Dan Ward <ward1516@stthomas.edu>
2017-09-23 23:08:45 -0500

    updated for class project

--- TP_1_starting/src/Copy.java
+++ TP_1_starting/src/Copy.java
@@ -8,37 +8,67 @@
  * @author eric
  *
  */
-public class Copy
-{
+public class Copy {
+	public String getCopyID() {
+		return copyID;
+	}
+
+	public void setCopyID(String copyID) {
+		this.copyID = copyID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Patron getOutTo() {
+		return outTo;
+	}
+
+	public void setOutTo(Patron outTo) {
+		this.outTo = outTo;
+	}
+
 	private String copyID;
 	private String title;
 	private Patron outTo;
 
-	public Copy(String copyID, String title)
-	{
-		// finish this
+	public Copy(String copyID, String title) {
+		this.copyID = copyID;
+		this.title = title;
+//		FakeDB.putCopy(copyID,  title);
 	}
 
-	// generate getters and setters using Eclipse Source menu
-
-	public String toString()
-	{
-		// correctly implement this
-		return "A description of this Copy's current state.";
+	public String toString() {
+//		if (this.outTo == null) {
+//			return "" + this.title + " (ID: " + copyID + ") is not checked out.";
+//		} else {
+			return "" + this.title + " (ID: " + copyID + ")";// is checked out to " + this.getOutTo() + ".";
+//		}
 	}
 
 	@Override
-	public boolean equals(Object o)
-	{
-		// finish this: two are equals iff same copy ID
-
-		return false;
+	public boolean equals(Object o) {
+		
+		//Copy is equal if copyID string is equal
+		if (!(o instanceof Copy))
+			return false;
+		
+		Copy c = (Copy) o;
+		
+		if (c.copyID.equals(this.copyID)) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
-	// put test code in your main(), but realize it's not unit testing!
-
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Copy c1 = new Copy("C1", "Fun with Objects");
 		Patron p1 = new Patron("Eric", "P47");
 
@@ -46,5 +76,8 @@ public class Copy
 		System.out.println(p1);
 
 		Copy c2 = FakeDB.getCopy("C2");
+		
+		System.out.println(c2);
+		
 	}
 }
--- TP_1_starting/src/Patron.java
+++ TP_1_starting/src/Patron.java
@@ -12,60 +12,93 @@
 
 import java.util.ArrayList;
 
-public class Patron
-{
+public class Patron {
 	private String name;
 	private String patronID;
 	private ArrayList<Copy> copiesOut;
 
-	public Patron(String id, String name)
-	{
-		// finish this
+	public Patron(String id, String name) {
+		this.patronID = id;
+		this.name = name;
+		this.copiesOut = new ArrayList<Copy>();
 	}
 
-	public boolean checkCopyOut(Copy c)
-	{
-		// finish this
-
+	public boolean checkCopyOut(Copy c) {
+		c.setOutTo(this);
+		this.copiesOut.add(c);
 		return false;
 	}
 
-	public boolean checkCopyIn(Copy c)
-	{
-		// finish this
-		return false;
+	public boolean checkCopyIn(Copy c) {
+//		c.setOutTo(null);
+//		System.out.println("Before...");
+//		for (Copy copy : this.copiesOut) {
+//			System.out.println(copy.toString());
+//		}
+		this.copiesOut.remove(c);
+		return true;
+//		System.out.println("After...");
+//		for (Copy copy : this.copiesOut) {
+//			System.out.println(copy.toString());
+//		}
+//		return false;
 	}
 
 	@Override
-	public boolean equals(Object o)
-	{
-		// finish this: two are equals iff same patron ID
+	public boolean equals(Object o) {
+		
+		// finish this: two are equal iff same patron ID
 		if (!(o instanceof Patron))
 			return false;
 		
 		Patron p = (Patron) o;
-		//compare p.patronID with this.patronID  use .equals
 		
-		return false;
+		if (p.patronID.equals(this.patronID)) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
-	public String toString()
-	{
-		// finish this: return basic Patron info as String
-
-		return "A description of this Patron's current state.";
+	public String toString() {
+		String message = "Patron " + this.name + " (ID: " + patronID + ") currently has " + this.copiesOut.size() + " copies checked out.";
+		for (Copy c : this.copiesOut) {
+			message += " " + c.toString();
+		}
+		return message;
 	}
 
-	// put test code in your main(), but realize it's not unit testing!
+	public static void main(String[] args) {
+		Patron p1 = new Patron("P1", "Eric");
 
-	public static void main(String[] args)
-	{
-		Patron p1 = new Patron("P47", "Eric");
+		StdOut.println(p1);
 
-		System.out.println(p1);
-
-		Patron p2 = FakeDB.getPatron("P47");
+		Patron p2 = FakeDB.getPatron("P1");
 		Copy c1 = FakeDB.getCopy("C1");
+		Copy c2 = FakeDB.getCopy("C2");
+		
+		p1.checkCopyOut(c1);
+		StdOut.println(p1);
+		p1.checkCopyOut(c2);
+		StdOut.println(p1);
+		
+		p1.checkCopyIn(c2);
+		StdOut.println(p1);
+		p1.checkCopyIn(c1);
+		StdOut.println(p1);
+			
+//		
+//		Copy c3 = new Copy("C1", "Fun with Objects");
+//		Patron p3 = new Patron("Eric", "P47");
+//
+//		System.out.println(c3);
+//		System.out.println(p3);
+//
+//		Copy c4 = FakeDB.getCopy("C2");
+//		
+//		System.out.println(c4);
+		
 	}
 
 }
